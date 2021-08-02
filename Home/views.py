7577c7880526c8ef5ab793c41b3d03d4784f6cea
iamabhishekgawali <iamabhishekgawali@gmail.com>
2021-08-03 00:59:18 +0530

    Added right side text area and floating Submit button , Modified post request section in views.py

--- Home/views.py
+++ Home/views.py
@@ -2,10 +2,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def Convert(Description,tag,code,texteditor):
+    pass
+
 def home(request):
+    
+    text="Your Snippet will be generated here"
 
     if(request.method=='POST'):
-        val = request.POST.get("radiobtn")
-        print(val)
+        texteditor = request.POST.get("radiobtn")
+        Description = request.POST.get("description")
+        tag = request.POST.get("tag")
+        code = request.POST.get("code")
 
-    return render(request,"Home/index.html")
+
+        print(texteditor)
+        print(Description)
+        print(tag)
+        print(code)
+    
+    return render(request,"Home/index.html",{"values": text })
+
+
