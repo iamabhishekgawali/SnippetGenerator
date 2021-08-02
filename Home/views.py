63c883fa20c146d5c748b057495ba0f9b681a049
iamabhishekgawali <iamabhishekgawali@gmail.com>
2021-08-02 23:32:48 +0530

    Added and Styles Radio buttons for Vscode Sublime-Text and Atom

--- Home/views.py
+++ Home/views.py
@@ -1,7 +1,11 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-
 def home(request):
+
+    if(request.method=='POST'):
+        val = request.POST.get("radiobtn")
+        print(val)
+
     return render(request,"Home/index.html")
